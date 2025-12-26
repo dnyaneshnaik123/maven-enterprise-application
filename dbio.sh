2a66bd298fc0aabcee4d98c6dc1924bf77919bd1
dnyaneshnaik123 <128205110+dnyaneshnaik123@users.noreply.github.com>
2025-12-26 16:57:02 +0530

    Clean up dbio.sh by removing empty lines
    Removed multiple empty lines and retained existing content.

--- dbio.sh
+++ dbio.sh
@@ -5,11 +5,6 @@ whats new things
 new things in line
 new code is added
 remotely done the changes
-biodata
-self hosted k8s cluster
-docker compose file
-new linux command file is added
-git addede
 
 
 

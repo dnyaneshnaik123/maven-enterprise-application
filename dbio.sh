77a1c541255be536493f23b07f986275219f8f5a
dnyaneshnaik123 <128205110+dnyaneshnaik123@users.noreply.github.com>
2025-12-26 17:45:42 +0530

    Update dbio.sh

--- dbio.sh
+++ dbio.sh
@@ -1,10 +1,5 @@
 my name is dnyan
 quantum soft
 name is not exist
-my self dnyan
-whats new things
-new things in line
-remotely done the changes
-biodata
-my best wishes to u
-new line is added
+
+

e9b2efdadeb3764e6d60484db724f6a74135d2cf
dnyaneshnaik123 <128205110+dnyaneshnaik123@users.noreply.github.com>
2025-12-26 16:51:41 +0530

    Update dbio.sh with new content and remove old lines
    Removed old lines and added new content to dbio.sh.

--- dbio.sh
+++ dbio.sh
@@ -1,3 +1,5 @@
+new pwords to be added
+
 one more changes available
 whats new things
 new things in line
@@ -7,6 +9,7 @@ biodata
 self hosted k8s cluster
 docker compose file
 new linux command file is added 
+
 
 
 

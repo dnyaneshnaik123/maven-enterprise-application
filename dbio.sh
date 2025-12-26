ea25283935acaf7ac9f0cbe1cf6b433548d3a570
dnyaneshnaik123 <128205110+dnyaneshnaik123@users.noreply.github.com>
2025-12-26 16:31:39 +0530

    Remove redundant lines from dbio.sh

--- dbio.sh
+++ dbio.sh
@@ -1,6 +1,3 @@
-
-name is not exist
-my self dnyan
 whats new things
 new things in line
 remotely done the changes
@@ -8,6 +5,7 @@ biodata
 self hosted k8s cluster
 docker compose file
 new docker file is created
+
 
 
 

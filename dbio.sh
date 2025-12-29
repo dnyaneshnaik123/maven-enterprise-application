3889d0dcdfef4aa8db496b873b7d4425c797ef19
dnyaneshnaik123 <128205110+dnyaneshnaik123@users.noreply.github.com>
2025-12-29 12:30:35 +0530

    Remove old lines and add Spring Boot application

--- dbio.sh
+++ dbio.sh
@@ -1,8 +1,18 @@
-new pwords to be added
-one more line added
-one more changes available
-hi
-new msg is addede
+
+
+
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
 
 
 
